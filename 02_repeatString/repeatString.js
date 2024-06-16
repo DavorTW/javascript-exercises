fd4ba0400330e278b549e330045f0a4c731134cb
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-06-16 22:48:11 +0800

    Finished exercise 01 and 02 with all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let i=0;
+    let repeatedString = '';
+    if (num < 0) {
+        return "ERROR";
+    }else{
+        while (i < num) {
+            repeatedString = repeatedString + string;
+            i++;
+        }
+        return repeatedString;
+    }
 };
 
 // Do not edit below this line
