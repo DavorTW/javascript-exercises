89430993495225f8a34b39bdb3fb71f2b71f05a7
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-06-17 10:18:52 +0800

    Finished exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    //we need to convert the string to an array so we can manipulate it
+    const stringArray = string.split("");
+    let reversedArray = [];
 
+    //we need to calculate the last index manually because js doesnt
+    // have a [-1] functionality
+    for(let i = stringArray.length - 1; i >= 0; i--){
+        reversedArray.push(stringArray[i]);
+    }
+    //we use join with empty quotes to concatenate each element
+    return reversedArray.join("");
 };
 
 // Do not edit below this line
