f2bb095b4f259f7e93439bf60e61b2a93b7843b7
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-07-21 16:49:22 +0800

    Finished exercise 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let strArr = [...str];
+   
+    let onlyLetters = strArr.filter(item =>{
+        return /[a-z0-9]/.test(item);
+    });
+    
+    let reversedArray = [];
+    for (let i = onlyLetters.length - 1; i >= 0; i--) {
+        reversedArray.push(onlyLetters[i])
+    }
+    
+    if (onlyLetters.length !== reversedArray.length) {
+        return false;
+    }else{
+        for(let i = 0; i < onlyLetters.length; i++){
+            if (onlyLetters[i] !== reversedArray[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
 
 };
 
