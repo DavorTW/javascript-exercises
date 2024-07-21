eb66ae8dcafb43737a9e2caca06a0f588a2e6a73
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-07-21 15:58:53 +0800

    Finished exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,51 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  // let sum = 0;
+	// for(let i=0; i<arr.length; i++){
+  //   sum += arr[i];
+  // }
+  // return sum;
+
+  let result = arr.reduce((total,currentItem) =>{
+    return total += currentItem
+  }, 0);
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let result = arr.reduce((total, currentItem)=>{
+    return total * currentItem;
+  },1);
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	let power = 1;
+  if (num2 === 0) {
+    return 1;
+  }else if (num2 === 1) {
+    return num1;
+  }else{
+    for(let i = 0; i<num2; i++){
+      power = power * num1;
+    }
+    return power;
+  }
+  
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+  return num * factorial(num - 1);
 };
 
 // Do not edit below this line
