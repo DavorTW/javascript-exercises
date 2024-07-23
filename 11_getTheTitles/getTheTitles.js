c9b796f6f4f0a79847a050a32d9f7d56b84683d1
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-07-24 01:51:18 +0800

    Added solution for exercise 11_getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,16 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // let titles = [];
+    // books.forEach(book => {
+    //     for (const key in book) {
+    //         if ( key === "title") {
+    //             titles.push(book[key]);
+    //         }
+    //     }
+    // });
+    
+    // return titles;
 
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
