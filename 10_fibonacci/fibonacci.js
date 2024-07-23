1600d4022a15956b0f23e2c347accdd82b566852
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2024-07-23 20:53:04 +0800

    Added solution for exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(nth) {
+    nth = Number(nth);
+    if (nth < 0) {
+        return "OOPS";
+    }else if (nth === 0) {
+        return 0;
+    }else if (nth >0 && nth<= 2) {
+        return 1;
+    }
 
+    return fibonacci(nth-1) + fibonacci(nth-2);
 };
 
 // Do not edit below this line
